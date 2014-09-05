01371296ca255d7e06d4ddfa03fdecc233f89736
Chris <ludakris4@yahoo.com>
2014-09-05 15:52:44 -0600

    Initial Commit

---
+++ TimeCard-SwiftTests/TimeCard_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  TimeCard_SwiftTests.swift
+//  TimeCard-SwiftTests
+//
+//  Created by chris mitchell on 9/5/14.
+//  Copyright (c) 2014 axialgrafiks. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TimeCard_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
